76987861577da3ac3eca8b6ed915f7bfe3ca0653
Adminfoco <Adminfoco@ML-RefVm-282052>
2023-09-11 21:56:18 -0500

    Servicio

---
+++ InvoiceService/IInvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace InvoiceService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IInvoiceService
+    {
+
+        [OperationContract]
+        IEnumerable<CustomerInvoice> GetInvoice(int invoicecode, string email);
+
+        [OperationContract]
+        Task<IEnumerable<CustomerInvoice>> AsyncGetInvoice(int invoicecode, string email);
+    }
+}
---
+++ InvoiceService/InvoiceService.svc.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess;
+using Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class InvoiceService : IInvoiceService
+    {
+        public async Task<IEnumerable<CustomerInvoice>> AsyncGetInvoice(int invoicecode, string email)
+        {
+            return await Task<IEnumerable<CustomerInvoice>>.Factory.StartNew(() =>
+            {
+                using (var unitOfWork = new UnitOfWork(new ChinookContext()))
+                {
+                    return unitOfWork.Customers.Customerinvoice(invoicecode, email).ToList();
+                }
+            });
+        }
+
+        public IEnumerable<CustomerInvoice> GetInvoice(int invoicecode, string email)
+        {
+            using (var unitOfWork = new UnitOfWork(new ChinookContext()))
+            {
+                return unitOfWork.Customers.Customerinvoice(invoicecode, email).ToList();
+            }
+        }
+    }
+}
